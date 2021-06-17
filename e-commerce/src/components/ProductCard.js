7ee27dc7c6a5e3912fb19e361e52b013c31ac63f
Victor Farina <victor.farina@mail.com>
2021-06-17 23:00:30 +0200

    fixed productCard

--- e-commerce/src/components/ProductCard.js
+++ e-commerce/src/components/ProductCard.js
@@ -1,18 +1,13 @@
 import React from 'react'
-
 const ProductCard = ({product}) => {
-
     return (
-
         <div className="product-card">
             <img src={product.img} alt="" />
             <h1>{product.name}</h1>
             <h3>fr {product.price} kr</h3>
             <button>Buy</button>
         </div>
-        
     )
-    
 }
 
 export default ProductCard
