1de741a4e13e724d63171f9d8f2233edf8676ada
Victor Farina <victor.farina@mail.com>
2021-07-02 17:03:43 +0200

    protected userpage fixed

--- e-commerce/src/views/User.js
+++ e-commerce/src/views/User.js
@@ -1,56 +1,40 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 
 
-const User = (props) => {
 
-    const activeUser = props.location.activeUser.activeUser;
-
+const User = ({activeUser,loggedIn}) => {
     console.log(activeUser);
-
-
-
-    
-
-
     
     
+  
+
     return (
 
         <section className=" container d-flex mt-5">
-
             <div className="w-25 card border">
+                <ul className="list-unstyled mx-5">
 
-                <ul className=" list-unstyled">
-                    <li className="my-5">Tidigare beställningar</li>
-                    <li className="mb-5">Mina kontakt uppgifter</li>
-                    <li className="mb-5">Mina betalningsuppgifter</li>
+                    <li className="my-5 shadow-4 p-3">Tidigare beställningar</li>
+                  
+                    <li className="my-5 shadow-4 p-3">Mina kontakt uppgifter</li>
+
+                    <li className="my-5 shadow-4 p-3">Mina betalningsuppgifter</li>
+
                 </ul>
-
             </div>
-
             <div className="ms-5 mt-5">
-              <h1>Hej {activeUser.firstName}!</h1>
-              <p>Här kan du se alla dina tidigare beställningar!</p>
 
-
-
-
-
-            
-              <p>ordernr:{activeUser.orders}</p>
-              <p>e-post:{activeUser.email}</p>
-              <p>e-post:{activeUser.password}</p>
-
-
-
-
-
+                <h1>Hej {activeUser.firstName} !</h1>
+                <small>Här kan du se dina beställningar och användaruppgifter!</small>
 
 
 
             </div>
         </section>
+        
     )
 }
 
